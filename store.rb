91ca090860da7b6c6a87c18a4668b0e94573922b
“FirstofHerName” <mbe773@gmail.com>
2017-08-22 00:19:27 -0500

    store

---
+++ store.rb
@@ -0,0 +1,5 @@
+
+  store_item_1 = {owner: "Elia", ice_cream: "oreo", price: 5, size: "gallon"}
+  store_item_2 = {:owner => "Elia", :ice_cream => "strawberry", :price => 3, :size => "pint"}
+
+  puts "#{store_item_1[:owner]} #{:ice_cream} #{:price} #{:size}"
