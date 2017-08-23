93001c86028d05559fd0ed978653e0f515087f76
“FirstofHerName” <mbe773@gmail.com>
2017-08-23 17:47:26 -0500

    tuesday

--- emplyee.rb
+++ emplyee.rb
@@ -9,22 +9,30 @@
 
 
 
-
-
-
-
-
-
-
 class Employee
+  attr_reader :first_name, :last_name, :active
+  attr_writer :act
   def initialize(input_first_name, input_last_name, inout_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
   end
+
   def first_name
+    @first_name  #reader method
+  end
+
+  def last_name
     @first_name
+  end
+
+  def active
+    @active
+  end
+
+  def active=(new_active)
+    @active = new_active  #writer method
   end
 
   def print_info
@@ -37,7 +45,40 @@ end
 
 
 
-    #employee_1 = Employee.new("Jimmy", "stewart", 70000, true)
-    #employee_2 = Employee.new("Susan", "Messing", 80000, true)
-    #p employee_1
-    #p employee_2
+    employee_1 = Employee.new(
+                               first_name: "Jimmy", 
+                               last_name: "stewart", 
+                               salary: 70000, 
+                               active: true
+                               )
+    employee_2 = Employee.new(
+                              first_name: "Susan", 
+                              last_name: "Messing", 
+                              salary: 80000, 
+                              active: true
+                              )
+    
+    p employee_2.active
+    employee_2.active = false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
--- store.rb
+++ store.rb
@@ -1,5 +1,66 @@
 
-  store_item_1 = {owner: "Elia", ice_cream: "oreo", price: 5, size: "gallon"}
-  store_item_2 = {:owner => "Elia", :ice_cream => "strawberry", :price => 3, :size => "pint"}
+  #store_item_1 = {owner: "Elia", ice_cream: "oreo", price: 5, size: "gallon"}
+  #store_item_2 = {:owner => "Elia", :ice_cream => "strawberry", :price => 3, :size => "pint"}
 
-  puts "#{store_item_1[:owner]} #{:ice_cream} #{:price} #{:size}"
+  #puts "#{store_item_1[:owner]} #{:ice_cream} #{:price} #{:size}"
+
+
+
+class Store
+  attr_reader :store_owner, :price
+  attr_writer :ice_cream, :size
+
+  def initialize (input_options)
+    @store_owner = input_options[:owner]
+    @ice_cream = input_options[:ice_cream]
+    @price = input_options[:price]
+    @size = input_options[:size]
+  end
+
+  def store_owner
+    @store_owner
+  end
+
+  def ice_cream=(new_ice_cream)
+    @ice_cream = new_ice_cream
+  end
+
+  def price
+    @price
+  end
+
+  def size=(new_size)
+    @size = new_size
+  end
+end
+
+class Food < Store
+  attr_reader :shelf_life
+  def initialize(input_options)
+    super
+    @shelf_life = input_options[:shelf_life]
+  end
+
+end
+
+store_item_1 = Store.new(
+                          owner: "Elia", 
+                          ice_cream: "oreo", 
+                          price: 5, 
+                          size: "gallon"
+                          )
+store_item_2 = Store.new(
+                          :owner => "Elia", 
+                          :ice_cream => "strawberry", 
+                          :price => 3, 
+                          :size => "pint"
+                          )
+p store_item_1.store_owner
+p store_item_2
+
+food= Food.new(owner: "Elia", ice_cream: "oreo", price: 5, size: "gallon", shelf_life: 2)
+
+p food.shelf_life
+
+
+
